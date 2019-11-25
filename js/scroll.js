bb94efcaa502138ff37336413178cf380d9ce813
Bhawna Agarwal <bhawna2109@users.noreply.github.com>
2019-11-24 22:57:26 -0500

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,48 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    if(st==0){
+          $('header').css("box-shadow", "none")
+    }
+
+    if(st!=0){
+      $('header').css("box-shadow", "0 0 10px rgba(0,0,0,0.4)")
+}
+
+    console.log(st);
+    
+    lastScrollTop = st;
+}
